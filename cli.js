4f439ec7d0421be43ea039e1f238672903a2d2e5
Tim Douglas <neurotech@gmail.com>
2016-02-06 17:59:17 +1100

    First major version functionality
     - Retrieve student/staff number associated with student/staff id.
     - Get current period

--- cli.js
+++ cli.js
@@ -1,2 +1,41 @@
-#!/usr/bin/env node
+#!/usr/bin/env DYLD_LIBRARY_PATH=node_modules/ibm_db/installer/clidriver/lib/icc node
 'use strict';
+const updateNotifier = require('update-notifier');
+const meow = require('meow');
+const chalk = require('chalk');
+const edumate = require('./edumate');
+const pkg = require('./package.json');
+
+const help = `
+  Usage
+  -----
+    $ ${chalk.yellow('edumate')} ${chalk.blue('<command>')} ${chalk.white('<value>')}
+
+  Commands
+  --------
+    ${chalk.blue('student')} ${chalk.white('<STUDENT_ID>')}
+      Returns the STUDENT_NUMBER associated with STUDENT_ID.
+
+    ${chalk.blue('staff')} ${chalk.white('<STAFF_ID>')}
+      Returns the STAFF_NUMBER associated with STAFF_ID.
+
+    ${chalk.blue('period')}
+      Returns the current week, period, and end time of period.
+`;
+const cli = meow(help);
+
+if (cli.input.length === 0) {
+  console.log(`  ${chalk.red('No arguments specified!')} \n ${help}`);
+} else {
+  if (cli.input[0] === 'student') {
+    edumate.studentId(cli.input[1]);
+  }
+  if (cli.input[0] === 'staff') {
+    edumate.staffId(cli.input[1]);
+  }
+  if (cli.input[0] === 'period') {
+    edumate.period();
+  }
+}
+
+updateNotifier({pkg}).notify();
---
+++ edumate.js
@@ -0,0 +1,58 @@
+'use strict';
+const db = require('node-edumate');
+const logSymbols = require('log-symbols');
+const chalk = require('chalk');
+const edumate = {};
+
+const config = {
+  host: process.env.EDUMATE_HOST,
+  port: process.env.EDUMATE_PORT,
+  suffix: process.env.EDUMATE_PATH,
+  username: process.env.EDUMATE_USERNAME,
+  password: process.env.EDUMATE_PASSWORD
+};
+
+edumate.studentId = function (id) {
+  var sql = `SELECT student_number FROM EDUMATE.student WHERE student_id = ${id}`;
+  db.query(config, sql, {clean: true})
+    .then(results => {
+      if (results.length === 0) { _noResults('student'); } else {
+        console.log(results[0].studentNumber);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+edumate.staffId = function (id) {
+  var sql = `SELECT staff_number FROM EDUMATE.staff WHERE staff_id = ${id}`;
+  db.query(config, sql, {clean: true})
+    .then(results => {
+      if (results.length === 0) { _noResults('staff'); } else {
+        console.log(results[0].staffNumber);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+edumate.period = function () {
+  var sql = `SELECT * FROM DB2INST1.view_api_v1_periods WHERE current = 1`;
+  db.query(config, sql, {clean: true})
+    .then(results => {
+      if (results.length === 0) { _noResults('period'); } else {
+        console.log(`We are in week ${chalk.blue(results[0].week)}.\nIt's currently ${chalk.yellow(results[0].period.toLowerCase())}.\nThis period will end at ${chalk.magenta(results[0].endTime)}.`);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+function _noResults (type) {
+  console.log(`${logSymbols.error} No ${type} results.`);
+}
+
+module.exports = edumate;
